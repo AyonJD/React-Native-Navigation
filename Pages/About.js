24928c77ee6c8289612c8b00b06075d9391280cb
Ayon Jodder <ayonjodder177@gmail.com>
2022-10-11 23:00:15 +0600

    route and navigation

--- Pages/About.js
+++ Pages/About.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
-const About = () => {
+const About = ({ route, navigation }) => {
+    // route.params is used to get the data passed from the previous component
+    const { myName } = route.params;
     return (
         <>
             <View>
                 <Text>This is About page</Text>
+                <Text>My name is {myName}</Text>
+                <Button title='Go Back' onPress={() => navigation.goBack()} />
             </View>
         </>
     );
--- Pages/Home.js
+++ Pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View, Button } from 'react-native';
 
+// navigation and route are passed as props from App.js defaultly
 const Home = ({ navigation }) => {
+    // navigation.navigate('component name') is used to navigate to that component
 
+    const [name, setName] = useState('Ayon Jodder');
     const handleRedirect = () => {
-        navigation.navigate('About');
+        navigation.navigate('About', { myName: `${name}` });
     }
     return (
         <>
